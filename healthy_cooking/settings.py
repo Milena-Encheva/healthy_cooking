c1c5baf3f78b84494a793e4f74eac8580093a9cc
Milena_Encheva <encheva.milena@gmail.com>
2024-04-10 21:04:35 +0300

    Added settings for Azure

--- healthy_cooking/settings.py
+++ healthy_cooking/settings.py
@@ -67,16 +67,29 @@ WSGI_APPLICATION = 'healthy_cooking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "healthy_cooking",
-        "USER": "milenae",
-        "PASSWORD": "me12345",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "healthy_cooking",
+            "USER": "milenae",
+            "PASSWORD": "me12345",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        }
+    }
 
 
 # Password validation
@@ -126,6 +139,8 @@ STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = (
     BASE_DIR / "staticfiles",
 )
+
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
